ad84bc6372ac0c78ddb42be29e6d3b444bb3bc41
Patrick Drechsler <patrick@pdrechsler.de>
2021-09-01 21:31:20 +0200

    layout: fix footer for multiple presenters

--- mathema-2021/layouts/default-with-footer.vue
+++ mathema-2021/layouts/default-with-footer.vue
@@ -2,17 +2,12 @@
   <div class="slidev-layout default-with-footer">
     <div class="my-auto w-full">
       <slot />
-
       <div class="w-w-10/12 fixed bottom-0 flex justify-between mb-2 text-sm">
         <span>{{ $slidev.configs.occasion }} </span>
-        <span>{{ $slidev.configs.company }} - {{ $slidev.configs.presenter }} - {{ $slidev.configs.contact }}</span>
+        <span v-if="$slidev.configs.presenter">{{ $slidev.configs.company }} - {{ $slidev.configs.presenter }} - {{ $slidev.configs.contact }}</span>
+        <span v-else>{{ $slidev.configs.company }} - {{ $slidev.configs.contact }}</span>
         <span><SlideCurrentNo /> / <SlidesTotal /></span>
-          
-          
-          
         </div>
     </div>
-
-    
   </div>
 </template>
